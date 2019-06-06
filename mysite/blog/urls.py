ad980e9fdaf5f70630a6bebb4d189fc48b0e712a
Halifay <mihail52sh@mail.ru>
2019-06-06 12:12:52 +1000

    going to learn html)

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:post_id>/', views.post_info, name='post_info'),
-    path('<int:post_id>/comments/', views.comments_view, name='comments_view'),
+    path('detail/<int:post_id>/', views.post_info, name='post_info'),
+    path('', views.comments_view, name='comments_view'),
 
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Post, Comment
 
 
 def index(request):
     latest_posts_list = Post.objects.order_by('-published_date')
-    out = ', '.join([q.title for q in latest_posts_list])
-    return HttpResponse(out)
+    context = {'latest_posts_list': latest_posts_list, }
+    return render(request, 'blog/index.html', context)
+
 
 def post_info(request, post_id):
-    response = "This is post %s." % post_id
-    return HttpResponse(response)
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'blog/detail.html', {'post': post})
 
 
 def comments_view(requiest, post_id):
